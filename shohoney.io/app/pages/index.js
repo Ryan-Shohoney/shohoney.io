32651b13e853cda2ac0d6d157b936dcb75e18818
Ryan Shohoney <ryguy9829@gmail.com>
2019-10-23 23:56:42 -0500

    Now running KeystoneJS5 with Next.js app on top.

---
+++ shohoney.io/app/pages/index.js
@@ -0,0 +1,9 @@
+const Index = _ => (
+  <div>
+    <h1>
+      Hello, from Next.js
+    </h1>
+  </div>
+);
+
+export default Index;
---
+++ shohoney.io/index.js
@@ -0,0 +1,67 @@
+const { Keystone } = require('@keystonejs/keystone');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Text, Checkbox, Password } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NextApp } = require('@keystonejs/app-next');
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+
+const PROJECT_NAME = "shohoney.io";
+
+process.env.KNEX_URI = 'postgres://ryguy:password123@localhost:5432/shohoney_io';
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter()
+});
+
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
+  return { id: user.id };
+};
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
+
+keystone.createList('User', {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true,
+    },
+    isAdmin: { type: Checkbox },
+    password: {
+      type: Password,
+    },
+  },
+  // To create an initial user you can temporarily remove access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true,
+  },
+});
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    // To create an initial user you can temporarily remove the authStrategy below
+    new AdminUIApp({  authStrategy }),
+    new NextApp({dir: 'app'})
+  ],
+};
