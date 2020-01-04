7df5d88fb5ee378550ac8a4f1ef410b679494b79
Ryan Shohoney <ryguy9829@gmail.com>
2020-01-04 00:00:46 -0600

    Just cleaning up

--- shohoney.io/app/pages/wedding/index.js
+++ shohoney.io/app/pages/wedding/index.js
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import Header from '../../components/common/header';
+import { Slider, Slide } from 'react-materialize';
 
 export default function Index() {
   return (
     <div>
-      <Header/>
-      <Link href="/">
-        <a>
-          &lt; Back Home
-        </a>
-      </Link>
-      <h1>
-        This is the landing page for the wedding.
-      </h1>
+      <Header />
+
+      <Slider
+        fullscreen={false}
+        options={{
+          height: 600,
+          indicators: false
+        }}
+      >
+        <Slide image={<img src="madison-night.jpg" />}>
+
+        </Slide>
+      </Slider>
     </div>
   )
 }
